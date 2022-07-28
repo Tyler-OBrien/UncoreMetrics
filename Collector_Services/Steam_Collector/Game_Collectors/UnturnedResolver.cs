7898b1173befc38a9a524b7dc24d6f2dacd81b5f
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-27 23:21:37 -0400

    Add Unturned Server and Resolver

---
+++ Collector_Services/Steam_Collector/Game_Collectors/UnturnedResolver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Options;
+using Steam_Collector.Helpers;
+using Steam_Collector.Models;
+using Steam_Collector.Models.Games.Steam.SteamAPI;
+using Steam_Collector.SteamServers;
+using UncoreMetrics.Data;
+using UncoreMetrics.Data.GameData.Unturned;
+
+namespace Steam_Collector.Game_Collectors;
+
+public class UnturnedResolver : BaseResolver
+{
+    public UnturnedResolver(
+        IOptions<BaseConfiguration> baseConfiguration, ServersContext serversContext, ISteamServers steamServers) :
+        base(baseConfiguration, serversContext, steamServers)
+    {
+    }
+
+
+    public override string Name => "Unturned";
+    public override ulong AppId => 304930;
+
+
+    public override async Task HandleServersGeneric(List<IGenericServerInfo> servers)
+    {
+        var customServers = new List<UnturnedServer>(servers.Select(ResolveServerDetails));
+        await Submit(customServers);
+    }
+
+    private UnturnedServer ResolveServerDetails(IGenericServerInfo server)
+    {
+        UnturnedServer customServer = new();
+
+        if (server.ExistingServer != null)
+            customServer.Copy(server.ExistingServer);
+
+        if (server.ServerRules != null) customServer.ResolveGameDataPropertiesFromRules(server.ServerRules);
+
+        return customServer;
+    }
+}
---
+++ Data_Services/UncoreMetrics.Data/GameData/Unturned/UnturnedServer.cs
@@ -0,0 +1,28 @@
+﻿namespace UncoreMetrics.Data.GameData.Unturned;
+
+public class UnturnedServer : Server
+{
+    [GameDataRulesProperty("Browser_Desc_Full_Line_{0}")]
+    public string? BrowserDescription { get; set; }
+
+
+    [GameDataRulesProperty("Browser_Desc_Hint")]
+    public string? BrowserDescriptionHint { get; set; }
+
+    [GameDataRulesProperty("Browser_Icon")]
+    public string? BrowserIcon { get; set; }
+
+
+    [GameDataRulesProperty("Custom_Link_Message_{0}")]
+    public List<string>? CustomLinks { get; set; }
+
+
+    [GameDataRulesProperty("GameVersion")] public string? GameVersion { get; set; }
+
+
+    [GameDataRulesProperty("Mod_{0}", ValueType.Running)]
+    public string? Mods { get; set; }
+
+    [GameDataRulesProperty("rocketplugins")]
+    public string? RocketPlugins { get; set; }
+}
