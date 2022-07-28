26705965ece8c0cf9a5158259b3edafd5cd6fb1e
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-27 23:20:07 -0400

    Update ARK Resolver

--- Collector_Services/Steam_Collector/Game_Collectors/ArkResolver.cs
+++ Collector_Services/Steam_Collector/Game_Collectors/ArkResolver.cs
@@ -8,89 +8,33 @@ using UncoreMetrics.Data.GameData.ARK;
 
 namespace Steam_Collector.Game_Collectors;
 
-public class ArkResolver : BaseResolver
+public class ARKResolver : BaseResolver
 {
-
-    public ArkResolver(
+    public ARKResolver(
         IOptions<BaseConfiguration> baseConfiguration, ServersContext serversContext, ISteamServers steamServers) :
         base(baseConfiguration, serversContext, steamServers)
     {
     }
 
 
-
-
-
-
     public override string Name => "ARK";
     public override ulong AppId => 346110;
 
 
-
-
-
     public override async Task HandleServersGeneric(List<IGenericServerInfo> servers)
     {
-
-        List<ArkServer> arkServers = new List<ArkServer>(servers.Select(ResolveServerDetails));
+        var arkServers = new List<ArkServer>(servers.Select(ResolveServerDetails));
         await Submit(arkServers);
     }
 
     private ArkServer ResolveServerDetails(IGenericServerInfo server)
     {
-        ArkServer arkServer = new ArkServer();
+        var arkServer = new ArkServer();
 
         if (server.ExistingServer != null)
             arkServer.Copy(server.ExistingServer);
 
-        if (server.ServerRules != null)
-        {
-            arkServer.ResolveGameDataPropertiesFromRules(server.ServerRules);
-
-            if (server.ServerRules.TryGetBooleanExtended("ALLOWDOWNLOADCHARS_i", out var allowDownloadChars))
-                arkServer.DownloadCharacters = allowDownloadChars;
-
-
-            if (server.ServerRules.TryGetBooleanExtended("ALLOWDOWNLOADITEMS_i", out var allowDownloadItems))
-                arkServer.DownloadItems = allowDownloadItems;
-
-
-            if (server.ServerRules.TryGetBooleanExtended("OFFICIALSERVER_i", out var officialServer))
-                arkServer.OfficialServer = officialServer;
-
-            if (server.ServerRules.TryGetBooleanExtended("SESSIONISPVE_i", out var PVEServer))
-                arkServer.PVE = PVEServer;
-
-
-            if (server.ServerRules.TryGetBooleanExtended("SERVERUSESBATTLEYE_b", out var hasBattleye))
-                arkServer.Battleye = hasBattleye;
-
-
-            if (server.ServerRules.TryGetBooleanExtended("ServerPassword_b", out var hasPassword))
-                arkServer.PasswordRequired = hasPassword;
-
-            if (server.ServerRules.TryGetBooleanExtended("HASACTIVEMODS_i", out var hasMods))
-                arkServer.Modded = hasMods;
-
-
-            if (server.ServerRules.TryGetInt("DayTime_s", out var daysRunning))
-                arkServer.DaysRunning = daysRunning;
-
-
-            if (server.ServerRules.TryGetInt("SESSIONFLAGS", out var sessionFlags))
-                arkServer.SessionFlags = sessionFlags;
-
-
-            if (server.ServerRules.TryGetString("ClusterId_s", out var clusterId))
-                arkServer.ClusterID = clusterId;
-
-
-            if (server.ServerRules.TryGetString("CUSTOMSERVERNAME_s", out var customServerName))
-                arkServer.CustomServerName = customServerName;
-
-
-            arkServer.Mods = server.ServerRules.TryGetRunningList("MOD{0}_s");
-        }
+        if (server.ServerRules != null) arkServer.ResolveGameDataPropertiesFromRules(server.ServerRules);
 
         return arkServer;
     }
