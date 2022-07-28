92dc77e7a51c9900c9dd0e13a891c0e710a0d45f
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-27 23:20:44 -0400

    Add DayZ Server and Resolver

---
+++ Collector_Services/Steam_Collector/Game_Collectors/DayZResolver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Options;
+using Steam_Collector.Helpers;
+using Steam_Collector.Models;
+using Steam_Collector.Models.Games.Steam.SteamAPI;
+using Steam_Collector.SteamServers;
+using UncoreMetrics.Data;
+using UncoreMetrics.Data.GameData.DayZ;
+
+namespace Steam_Collector.Game_Collectors;
+
+public class DayZResolver : BaseResolver
+{
+    public DayZResolver(
+        IOptions<BaseConfiguration> baseConfiguration, ServersContext serversContext, ISteamServers steamServers) :
+        base(baseConfiguration, serversContext, steamServers)
+    {
+    }
+
+
+    public override string Name => "DayZ";
+    public override ulong AppId => 221100;
+
+
+    public override async Task HandleServersGeneric(List<IGenericServerInfo> servers)
+    {
+        var customServers = new List<DayZServer>(servers.Select(ResolveServerDetails));
+        await Submit(customServers);
+    }
+
+    private DayZServer ResolveServerDetails(IGenericServerInfo server)
+    {
+        DayZServer customServer = new();
+
+        if (server.ExistingServer != null)
+            customServer.Copy(server.ExistingServer);
+
+        if (server.ServerRules != null) customServer.ResolveGameDataPropertiesFromRules(server.ServerRules);
+
+        return customServer;
+    }
+}
---
+++ Data_Services/UncoreMetrics.Data/GameData/DayZ/DayZServer.cs
@@ -0,0 +1,19 @@
+﻿namespace UncoreMetrics.Data.GameData.DayZ;
+
+public class DayZServer : Server
+{
+    [GameDataRulesProperty("allowedBuild")]
+    public bool? AllowedBuild { get; set; }
+
+    [GameDataRulesProperty("island")] public string? Island { get; set; }
+
+    [GameDataRulesProperty("language")] public string? Language { get; set; }
+
+    [GameDataRulesProperty("requiredBuild")]
+    public int? RequiredBuild { get; set; }
+
+    [GameDataRulesProperty("requiredVersion")]
+    public int? RequiredVersion { get; set; }
+
+    [GameDataRulesProperty("timeLeft")] public int? TimeLeft { get; set; }
+}
