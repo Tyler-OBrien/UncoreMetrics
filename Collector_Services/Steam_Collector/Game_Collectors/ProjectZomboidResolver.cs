467e8a26903739ee97a1af7f41a53f23da298d10
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-27 23:21:13 -0400

    Add Project Zomboid Server and Resolver

---
+++ Collector_Services/Steam_Collector/Game_Collectors/ProjectZomboidResolver.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using Steam_Collector.Helpers;
+using Steam_Collector.Models;
+using Steam_Collector.Models.Games.Steam.SteamAPI;
+using Steam_Collector.SteamServers;
+using UncoreMetrics.Data;
+using UncoreMetrics.Data.GameData.ProjectZomboid;
+
+namespace Steam_Collector.Game_Collectors;
+
+public class ProjectZomboidResolver : BaseResolver
+{
+    public ProjectZomboidResolver(
+        IOptions<BaseConfiguration> baseConfiguration, ServersContext serversContext, ISteamServers steamServers) :
+        base(baseConfiguration, serversContext, steamServers)
+    {
+    }
+
+
+    public override string Name => "ProjectZomboid";
+    public override ulong AppId => 108600;
+
+
+    public override async Task HandleServersGeneric(List<IGenericServerInfo> servers)
+    {
+        var projectZomboidServers = new List<ProjectZomboidServer>(servers.Select(ResolveServerDetails));
+        await Submit(projectZomboidServers);
+    }
+
+    private ProjectZomboidServer ResolveServerDetails(IGenericServerInfo server)
+    {
+        ProjectZomboidServer customServer = new();
+
+        if (server.ExistingServer != null)
+            customServer.Copy(server.ExistingServer);
+
+
+        if (server.ServerRules != null) customServer.ResolveGameDataPropertiesFromRules(server.ServerRules);
+
+        return customServer;
+    }
+}
---
+++ Data_Services/UncoreMetrics.Data/GameData/ProjectZomboid/ProjectZomboidServer.cs
@@ -0,0 +1,24 @@
+﻿namespace UncoreMetrics.Data.GameData.ProjectZomboid;
+
+public class ProjectZomboidServer : Server
+{
+    [GameDataRulesProperty("description")]
+    [GameDataRulesProperty("description:{0}/{1}", ValueType.Running, 1)]
+    public string? Description { get; set; }
+
+    [GameDataRulesProperty("modCount")] public int? ModCount { get; set; }
+
+    [GameDataRulesProperty("mods")] public string? Mods { get; set; }
+
+
+    [GameDataRulesProperty("open")] public bool? Open { get; set; }
+
+
+    [GameDataRulesProperty("public")] public bool? Public { get; set; }
+
+
+    [GameDataRulesProperty("pvp")] public bool? PvP { get; set; }
+
+
+    [GameDataRulesProperty("version")] public string? Version { get; set; }
+}
