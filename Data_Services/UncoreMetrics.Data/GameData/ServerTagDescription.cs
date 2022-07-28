aeb85aa00a8bec97d4d2a612c410c57400397695
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-27 22:34:57 -0400

    Add ServerTagDescription for describing what each tag equals

---
+++ Data_Services/UncoreMetrics.Data/GameData/ServerTagDescription.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UncoreMetrics.Data.GameData
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ServerTagDescription : Attribute
+    {
+        public ServerTagDescription(string name, string description, string toolTipDescription, string versionAdded)
+        {
+            Name = name;
+            Description = description;
+            ToolTipDescription = toolTipDescription;
+            VersionAdded = versionAdded;
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ToolTipDescription { get; set; }
+
+        public string VersionAdded { get; set; }
+    }
+}
