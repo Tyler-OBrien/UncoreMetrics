c2a70b55c8dc3952203e5851dc736845196a82ab
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-27 23:21:02 -0400

    Add Post Scriptum Server and Resolver

---
+++ Collector_Services/Steam_Collector/Game_Collectors/PostScriptumResolver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Options;
+using Steam_Collector.Helpers;
+using Steam_Collector.Models;
+using Steam_Collector.Models.Games.Steam.SteamAPI;
+using Steam_Collector.SteamServers;
+using UncoreMetrics.Data;
+using UncoreMetrics.Data.GameData.PostScriptum;
+
+namespace Steam_Collector.Game_Collectors;
+
+public class PostScriptumResolver : BaseResolver
+{
+    public PostScriptumResolver(
+        IOptions<BaseConfiguration> baseConfiguration, ServersContext serversContext, ISteamServers steamServers) :
+        base(baseConfiguration, serversContext, steamServers)
+    {
+    }
+
+
+    public override string Name => "PostScriptum";
+    public override ulong AppId => 736220;
+
+
+    public override async Task HandleServersGeneric(List<IGenericServerInfo> servers)
+    {
+        var customServers = new List<PostScriptumServer>(servers.Select(ResolveServerDetails));
+        await Submit(customServers);
+    }
+
+    private PostScriptumServer ResolveServerDetails(IGenericServerInfo server)
+    {
+        PostScriptumServer customServer = new();
+
+        if (server.ExistingServer != null)
+            customServer.Copy(server.ExistingServer);
+
+        if (server.ServerRules != null) customServer.ResolveGameDataPropertiesFromRules(server.ServerRules);
+
+        return customServer;
+    }
+}
---
+++ Data_Services/UncoreMetrics.Data/GameData/PostScriptum/PostScriptumServer.cs
@@ -0,0 +1,46 @@
+﻿namespace UncoreMetrics.Data.GameData.PostScriptum;
+
+public class PostScriptumServer : Server
+{
+    [GameDataRulesProperty("AllModsWhitelisted_b")]
+    public bool? AllModsWhitelisted { get; set; }
+
+    [GameDataRulesProperty("CurrentModLoadedCount_i")]
+    public int? CurrentModLoadedCount { get; set; }
+
+
+    [GameDataRulesProperty("Flags_i")] public int? Flags { get; set; }
+
+
+    [GameDataRulesProperty("GameMode_s")] public string? GameMode { get; set; }
+
+    [GameDataRulesProperty("GameVersion_s")]
+    public string? GameVersion { get; set; }
+
+
+    [GameDataRulesProperty("MatchTimeout_f")]
+    public int? MatchTimeout { get; set; }
+
+
+    [GameDataRulesProperty("Password_b")] public bool? Password { get; set; }
+
+
+    [GameDataRulesProperty("PlayerReserveCount_i")]
+    public int? PlayerReserveCount { get; set; }
+
+
+    [GameDataRulesProperty("PublicQueue_i")]
+    public int? PublicQueue { get; set; }
+
+
+    [GameDataRulesProperty("ReservedQueue_i")]
+    public int? ReservedQueue { get; set; }
+
+
+    [GameDataRulesProperty("SEARCHKEYWORDS_s")]
+    public string? SearchKeywords { get; set; }
+
+
+    [GameDataRulesProperty("SESSIONFLAGS")]
+    public int? SessionFlags { get; set; }
+}
