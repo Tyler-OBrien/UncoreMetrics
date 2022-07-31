411456b4cf04650014c0c0d497bdeeb498ef186c
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-31 16:35:11 -0400

    Add base configuration to Data models, include Clickhouse connection string

---
+++ Data_Services/UncoreMetrics.Data/Configuration/BaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UncoreMetrics.Data.Configuration
+{
+    public class BaseConfiguration
+    {
+        public string ClickhouseConnectionString { get; set; }
+
+    }
+}
